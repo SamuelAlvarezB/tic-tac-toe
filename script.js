d43dd5cf522a157f54eb1140ba884427988ead34
Samuel Alvarez <abrena2@gmail.com>
2025-11-19 17:56:07 +0100

    Improve logic of the game

--- script.js
+++ script.js
@@ -17,35 +17,40 @@ function game(board, players = []) {
     let player = players[0];
     let winner = "";
     let symbol = player.symbol;
+    let active = true;
     return {
         winner: winner,
         symbol: symbol,
         players: players,
+        player: player,
+        board: board,
+        active: active,
         showBoard() {
             console.log(board.board);
         },
         updateBoard(pos1, pos2) {
             this.getTurn();
             if (board.board[pos1][pos2] === "") {
-                board.board[pos1][pos2] = symbol;
-                if (symbol === 'X') {
-                    symbol = 'O';
-                } else {
-                    symbol = 'X';
-                }
+                board.board[pos1][pos2] = player.symbol;
+                this.checkWinCondition();
+                this.changePlayer();
             }
-            this.checkWinCondition();
-            this.changePlayer();
         },
         getTurn() {
-            console.log("Actual turn: " + player.name + "(" + symbol + ")");
+            let turnDiv = document.getElementById("turn");
+            if (this.active) {
+                turnDiv.innerText = "Actual turn: " + player.name + "(" + player.symbol + ")";
+            } else {
+                turnDiv.innerText = "Game Finished"
+            }
         },
         changePlayer() {
-            if (player === players[0]) {
+            if (player.symbol === "X") {
                 player = players[1];
-            } else {
+            } else if (player.symbol === "O") {
                 player = players[0];
             }
+            this.getTurn();
         },
         checkWinCondition() {
             let row1Check = [board.board[0][0], board.board[0][1], board.board[0][2]];
@@ -54,6 +59,8 @@ function game(board, players = []) {
             let col1Check = [board.board[0][0], board.board[1][0], board.board[2][0]];
             let col2Check = [board.board[0][1], board.board[1][1], board.board[2][1]];
             let col3Check = [board.board[0][2], board.board[1][2], board.board[2][2]];
+            let diag1Check = [board.board[0][0], board.board[1][1], board.board[2][2]];
+            let diag2Check = [board.board[0][2], board.board[1][1], board.board[2][0]];
             if (row1Check[0] === row1Check[1] && row1Check[1] === row1Check[2] && row1Check[0] != "" && row1Check[1] != "" && row1Check[2] != "") {
                 winner = player.name;
                 this.finishGame();
@@ -72,10 +79,87 @@ function game(board, players = []) {
             } else if (col3Check[0] === col3Check[1] && col3Check[1] === col3Check[2] && col3Check[0] != "" && col3Check[1] != "" && col3Check[2] != "") {
                 winner = player.name;
                 this.finishGame();
+            } else if (diag1Check[0] === diag1Check[1] && diag1Check[1] === diag1Check[2] && diag1Check[0] != "" && diag1Check[1] != "" && diag1Check[2] != "") {
+                winner = player.name;
+                this.finishGame();
+            } else if (diag2Check[0] === diag2Check[1] && diag2Check[1] === diag2Check[2] && diag2Check[0] != "" && diag2Check[1] != "" && diag2Check[2] != "") {
+                winner = player.name;
+                this.finishGame();
             }
         },
         finishGame() {
+            this.active = false;
+            this.getTurn();
             console.log("GAME FINISHED. WINNER IS: " + winner + "(" + player.symbol + ")");
+            const resetDiv = document.getElementById("reset");
+            console.log(resetDiv);
+            const winAnnounce = document.createElement("p");
+            winAnnounce.classList.add("win-announce");
+            winAnnounce.innerText = "The winner is: " + winner;
+            const confirmReset = document.createElement("p");
+            confirmReset.classList.add("confirm-reset");
+            confirmReset.innerText = "Do you want to restart the game?";
+            const resetButton = document.createElement("button");
+            resetButton.id = "resetButton";
+            resetButton.innerText = "RESET GAME";
+            resetButton.addEventListener('click', (e) => {
+                this.resetGame();
+            })
+            resetDiv.append(winAnnounce, confirmReset, resetButton);
+        },
+        deactiveBoard() {
+            if (active === false) {
+                console.log("DEACTIVATED")
+                let buttonArray = document.getElementsByClassName("gameCell");
+                for (const button of buttonArray) {
+                    button.disabled = true;
+                }
+            } else {
+                button.disabled = false;
+            }
+        },
+        resetGame() {
+            window.location.reload();
+        }
+    }
+}
+
+function displayBoard(game) {
+    return {
+        game: game,
+        createDisplay() {
+            const gameDiv = document.getElementById("game-board");
+            console.log(game.board.board);
+            for (let i = 0; i < game.board.board.length; i++) {
+                for (let j = 0; j < game.board.board[i].length; j++) {
+                    let newCell = document.createElement("button");
+                    newCell.innerHTML = game.board.board[i][j];
+                    newCell.classList.add("gameCell");
+                    newCell.classList.add(i + "-" + j);
+                    gameDiv.appendChild(newCell);
+                }
+            }
+            this.addListener();
+
+        },
+        addListener() {
+            let buttonArray = document.getElementsByClassName("gameCell");
+            for (const button of buttonArray) {
+                button.addEventListener('click', (e) => {
+                    if (game.active) {
+                        let splitArray = e.target.classList.item(1).split("-");
+                        let pos1 = splitArray[0];
+                        let pos2 = splitArray[1];
+                        game.updateBoard(pos1, pos2);
+                        e.target.innerText = game.board.board[pos1][pos2];
+                        if (game.board.board[pos1][pos2] === 'X') {
+                            e.target.style.color = "red";
+                        } else {
+                            e.target.style.color = "blue";
+                        }
+                    }
+                })
+            }
         }
     }
 }
@@ -99,15 +183,10 @@ let players = (function () {
 
 let newGame = (function () {
     let createGame = game(newBoard, players);
+    createGame.getTurn();
     return createGame;
 
 })();
 
-newGame.showBoard();
-newGame.updateBoard(0, 1);
-newGame.updateBoard(1, 1);
-newGame.updateBoard(0, 2);
-newGame.updateBoard(0, 0);
-newGame.updateBoard(1, 2);
-newGame.updateBoard(1, 0);
-newGame.updateBoard(2, 2);
+
+displayBoard(newGame).createDisplay();
